a6585a2d96a6b2f8a2a07deda6c525f21324cffd
aabodeif <ahmed.abodeif@trivago.com>
2021-04-26 18:00:21 +0200

    update docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+docker run \
+    hendrikmaus/nomad-cli \
+    nomad status \
+    -address=http://nomad-api.tcs.trv.cloud \
+    -token=csdOBYpjTFJqWoo teb-athene-web-55180-stage
